f408d8f929c5d96db21e42131d9640055abd0da6
hoangminhtit <23.volehoangminh12a7.2023nht@gmail.com>
2026-01-26 00:07:59 +0700

    Initial commit

---
+++ vis-core-fe/src/App.jsx
@@ -0,0 +1,15 @@
+import LoginPage from "./pages/LoginPage"
+
+import './index.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ vis-core-fe/src/index.js
@@ -0,0 +1,4 @@
+import logo_iuh from "./assets/logo-iuh.jpg"
+import logo_vis from "./assets/logo-vis.jpg"
+
+export { logo_iuh, logo_vis }
