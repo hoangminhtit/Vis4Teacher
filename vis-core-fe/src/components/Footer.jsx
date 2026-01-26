d5976e49f325a637b7621bea42293ebbbd62cade
hoangminhtit <23.volehoangminh12a7.2023nht@gmail.com>
2026-01-26 21:51:58 +0700

    feat: add navBar and Footer components

---
+++ vis-core-fe/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="text-center mb-2">
+                <p className="text-xs text-gray-400">
+                    © 2026 VisForTeacher. Phát triển bởi Đại học Công nghiệp TP.HCM
+                </p>
+            </div>
+        </>
+    )
+}
